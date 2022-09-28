a0db3b7cd968337a755e4769c3a12732450aaec5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-09-28 15:57:01 +0530

    error fixed

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -14,18 +14,46 @@ const AuthForm = () => {
   const submitHandler = (event) => {
     //setIsLogin(event.target.value);
     event.preventDefault(); //to prevent default sending request by browser
-    enteredEmail = emailInputRef.current.value;
-    enteredPassword = passwordInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
     //optional:add validation
     if (isLogin) {
     } else {
+      fetch(
+        "https://react-authentication-c1f21.firebaseio.com/message_list.json",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          //...
+        } else {
+          return res.json().then((data) => {
+            // console.log(data);
+            let errorMessage = "Authentication failed";
+            // if (data && data.error & data.error.message) {
+            //   //if error has data
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
+          });
+        }
+      });
     }
   };
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
           <input type="email" id="email" required ref={emailInputRef} />
